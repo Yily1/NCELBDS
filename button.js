528831f035be21480733f79b4e7ed9dca2ae8d0e
Yily1 <sajolgayel1@gmail.com>
2026-02-04 20:21:14 +0800

    change of navbar and added click button on navbar

--- button.js
+++ button.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   document.getElementById("btn").addEventListener("click", () => {
     alert("You clicked the button");
   });
+
+  document.getElementById("homeBtn").addEventListener("click", () => {
+    alert("Home clicked");
+  });
+
+  document.getElementById("chickensBtn").addEventListener("click", () => {
+    alert("Chickens clicked");
+  });
+
+  document.getElementById("reportBtn").addEventListener("click", () => {
+    alert("Report clicked");
+  });
+
 });
