e47e40bc22735a0b97a24cc92e3ad1e53b971ab2
Yelz Pogi <sajolgayel1@gmail.com>
2026-02-26 01:05:46 +0800

    modified

--- button.js
+++ button.js
@@ -4,16 +4,5 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("You clicked the button");
   });
 
-  document.getElementById("homeBtn").addEventListener("click", () => {
-    alert("Home clicked");
-  });
-
-  document.getElementById("chickensBtn").addEventListener("click", () => {
-    alert("Chickens clicked");
-  });
-
-  document.getElementById("reportBtn").addEventListener("click", () => {
-    alert("Report clicked");
-  });
 
 });
