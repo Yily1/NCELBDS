eb4dfe02223632a077035e2ede5388c0e77800d5
LasponaSilverioM <tawinlaspona@gmail.com>
2026-03-11 21:47:02 +0800

    added config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="chicken_behavior_db";
+$conn=mysqli_connect($host,$user,$password,$db);
+if(!$conn){
+die("Database connection failed");
+}
+?>
